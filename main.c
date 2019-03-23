2357e4bb1ac5c801e9fced9ff50005bc316bac57
cv007 <curtvm@msn.com>
2019-03-22 23:41:37 -0500

    initial commit of test code for pic32mm

--- main.c
+++ main.c
@@ -226,22 +226,18 @@ void main(void) {
 
     //display all brightness levels
     for(;;){
-    for(uint8_t i = 0; i < 64; i++ ){
-        disp_bright( disp_DIGIT0, i );
-        disp_bright( disp_DIGIT1, i );
-        disp_bright( disp_DIGIT2, i );
-        disp_number( i );
-        disp_show();
-        nco_waitms( 20 + 126 - i*2 );
-    }
-    for(uint8_t i = 63; i--; ){
-        disp_bright( disp_DIGIT0, i );
-        disp_bright( disp_DIGIT1, i );
-        disp_bright( disp_DIGIT2, i );
-        disp_number( i );
-        disp_show();
-        nco_waitms( 20 + 126 - i*2 );
-    }
+        int8_t j = 1;
+        for(uint8_t i = 0; ; ){
+            disp_bright( disp_DIGIT0, i );
+            disp_bright( disp_DIGIT1, i );
+            disp_bright( disp_DIGIT2, i );
+            disp_number( i );
+            disp_show();
+            nco_waitms( 20 + 126 - i*2 );
+            i += j;
+            if( i == 64 ){ j = -1; i = 63; }
+            else if( i == 0 ){ j = 1; }
+        }
     }
 
 
